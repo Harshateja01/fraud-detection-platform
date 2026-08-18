06dd9a5bfd267b4915f07a570e20c5c8af6aed4f
Harshateja01 <harshateja565@gmail.com>
2026-08-18 18:03:21 -0500

    Support Streamlit Cloud API configuration

--- src/app.py
+++ src/app.py
@@ -2,7 +2,16 @@ import os
 import requests
 import pandas as pd
 import streamlit as st
+API_BASE_URL = os.getenv(
+    "API_URL",
+    st.secrets.get(
+        "API_URL",
+        "http://127.0.0.1:8000"
+    )
+)
 
+API_URL = f"{API_BASE_URL}/score-transaction"
+HEALTH_URL = f"{API_BASE_URL}/health"
 
 # ============================================================
 # PAGE CONFIG
